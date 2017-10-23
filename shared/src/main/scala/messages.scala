9b0e7ef94b5a8f93da4a635a34cec28a3d9da2d0
Juan Pedro Moreno <juanpmorest@gmail.com>
2017-10-23 16:18:15 +0200

    Provides a dummy database and 4 dummy services

--- shared/src/main/scala/messages.scala
+++ shared/src/main/scala/messages.scala
@@ -1,17 +1,24 @@
 package metrifier
 
 case class Person(
-    id: PersonId,
+    id: String,
     name: PersonName,
     gender: String,
     location: Location,
     email: String,
     picture: Option[Picture])
 
-case class PersonId(name: String, value: String)
-
 case class PersonName(title: String, first: String, last: String)
 
 case class Location(street: String, city: String, state: String, postCode: Int)
 
 case class Picture(large: String, medium: String, thumbnail: String)
+
+case class PersonLink(p1: Person, p2: Person)
+
+case class DatabaseException(message: String, maybeCause: Option[Throwable] = None)
+    extends RuntimeException(message) {
+
+  maybeCause foreach initCause
+
+}
