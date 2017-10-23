6afae510832d6cde897018cb5bbc209127617ac1
Juan Pedro Moreno <juanpmorest@gmail.com>
2017-10-23 15:42:59 +0200

    Adds basic model

--- shared/src/main/scala/messages.scala
+++ shared/src/main/scala/messages.scala
@@ -1,4 +1,17 @@
 package metrifier
 
-case class Person(name: String, age: Int)
+case class Person(
+    id: PersonId,
+    name: PersonName,
+    gender: String,
+    location: Location,
+    email: String,
+    picture: Option[Picture])
 
+case class PersonId(name: String, value: String)
+
+case class PersonName(title: String, first: String, last: String)
+
+case class Location(street: String, city: String, state: String, postCode: Int)
+
+case class Picture(large: String, medium: String, thumbnail: String)
